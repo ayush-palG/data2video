ee63f5c66d3e7c5211658f4de3a72674fe211c03
ayush-palG <ayushpal.9470@gmail.com>
2025-04-01 18:31:12 +0530

    Save the input file as a ppm image

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <stdint.h>
+
+#define WIDTH  128
+#define HEIGHT 72
+
+typedef struct {
+  uint8_t *data;
+  size_t size;
+} Memory;
 
 size_t get_file_size(FILE *file)
 {
@@ -18,11 +27,6 @@ size_t get_file_size(FILE *file)
 
   return file_size;
 }
-
-typedef struct {
-  char *data;
-  size_t size;
-} Memory;
 
 void file_to_memory(const char *file_name, Memory *memory)
 {
@@ -41,6 +45,7 @@ void file_to_memory(const char *file_name, Memory *memory)
     memory->data[memory->size++] = temp;
   }
 
+  fclose(file);
   if (memory->size != file_size) {
     fprintf(stderr, "ERROR: could not read full file(%zu) into memory(%zu)\n", file_size, memory->size);
     exit(1);
@@ -54,11 +59,40 @@ void print_memory(const Memory *memory)
   }
 }
 
+void memory_save_as_ppm_file(Memory *memory, const char *file_name)
+{
+  FILE *file = fopen(file_name, "wb");
+  if (file == NULL) {
+    fprintf(stderr, "ERROR: could not open file `%s`: %s\n", file_name, strerror(errno));
+    exit(1);
+  }
+
+  fprintf(file, "P6\n%zu %zu 255\n", (size_t)WIDTH, (size_t)HEIGHT);
+  if (ferror(file)) {
+    fprintf(stderr, "ERROR: could not write to file `%s`: %s\n", file_name, strerror(errno));
+    exit(1);
+  }
+  
+  for(size_t i = 0; i < HEIGHT; ++i) {
+    for (size_t j = 0; j < WIDTH; ++j) {
+      for (size_t k = 0; k < 3; ++k) {
+	if (j < memory->size) {
+	  fprintf(file, "%c", memory->data[j]);
+	} else {
+	  fprintf(file, "%c", (char) 0);
+	}
+      }
+    }
+  }
+
+  fclose(file);
+}
+
 int main(void)
 {
   Memory memory = {0};
   file_to_memory("./input", &memory);
-  print_memory(&memory);
+  memory_save_as_ppm_file(&memory, "./output.ppm");
   
   return 0;
 }
