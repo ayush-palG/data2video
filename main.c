585e4f2e1a1cb1e713d39107115fabc0578f96a7
ayush-palG <ayushpal.9470@gmail.com>
2025-04-02 18:42:16 +0530

    Introduce box_pattern function

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <stdint.h>
 
-#define WIDTH  128
-#define HEIGHT 72
+#define WIDTH  1280
+#define HEIGHT 720
+#define COLOR_CHANNELS 3
 
 typedef struct {
   uint8_t *data;
@@ -73,14 +75,12 @@ void memory_save_as_ppm_file(Memory *memory, const char *file_name)
     exit(1);
   }
   
-  for(size_t i = 0; i < HEIGHT; ++i) {
-    for (size_t j = 0; j < WIDTH; ++j) {
-      for (size_t k = 0; k < 3; ++k) {
-	if (j < memory->size) {
-	  fprintf(file, "%c", memory->data[j]);
-	} else {
-	  fprintf(file, "%c", (char) 0);
-	}
+  for(size_t i = 0; i < HEIGHT*WIDTH; ++i) {
+    for (size_t j = 0; j < COLOR_CHANNELS; ++j) {
+      if (i < memory->size) {
+	fprintf(file, "%c", memory->data[i]);
+      } else {
+	fprintf(file, "%c", (char) 0);
       }
     }
   }
@@ -88,10 +88,38 @@ void memory_save_as_ppm_file(Memory *memory, const char *file_name)
   fclose(file);
 }
 
+void box_pattern(Memory *memory, size_t box_size)
+{
+  Memory new_mem = {0};
+  size_t height = (memory->size * box_size) / WIDTH;
+  height += !!((memory->size * box_size) % WIDTH != 0);
+  new_mem.size = box_size * box_size * WIDTH * height;
+  new_mem.data = calloc(WIDTH*HEIGHT*COLOR_CHANNELS, sizeof(char));
+  
+  size_t index = 0;
+  for (size_t h = 0; h < height; ++h) {
+    for (size_t i = 0; i < WIDTH/box_size; ++i) {
+      for (size_t j = 0; j < box_size; ++j) {
+	for (size_t k = 0; k < box_size; ++k) {
+	  if (index >= memory->size) goto done;
+	  new_mem.data[j*WIDTH+k + box_size*i + box_size*(WIDTH*h)] = memory->data[index];
+	}
+      }
+      index += 1;
+    }
+  }
+
+ done:
+  free(memory->data);
+  memory->data = new_mem.data;
+  memory->size = new_mem.size;
+}
+
 int main(void)
 {
   Memory memory = {0};
   file_to_memory("./input", &memory);
+  box_pattern(&memory, 4);
   memory_save_as_ppm_file(&memory, "./output.ppm");
   
   return 0;
