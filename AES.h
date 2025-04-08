1ce93f61910485813a5333bff9a296cb2e746abb
ayush-palG <ayushpal.9470@gmail.com>
2025-04-08 14:31:05 +0530

    (#1) Introduce initialize_aes_sbox

--- AES.h
+++ AES.h
@@ -11,6 +11,8 @@ uint8_t galois_mul(uint8_t a, uint8_t b);
 uint8_t galois_mul2(uint8_t a);
 uint8_t galois_mul3(uint8_t a);
 
+void initialize_aes_sbox(uint8_t sbox[256]);
+
 #endif // AES_H_
 
 #ifdef AES_IMPLEMENTATION
@@ -46,7 +48,33 @@ uint8_t galois_mul2(uint8_t a)
 
 uint8_t galois_mul3(uint8_t a)
 {
-  return a ^ gmul2(a);
+  return a ^ galois_mul2(a);
 }
+
+void initialize_aes_sbox(uint8_t sbox[256])
+{
+  uint8_t p = 1, q = 1;
+	
+  /* loop invariant: p * q == 1 in the Galois field */
+  do {
+    /* multiply p by 3 */
+    p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
+
+    /* divide q by 3 (equals multiplication by 0xf6) */
+    q ^= q << 1;
+    q ^= q << 2;
+    q ^= q << 4;
+    q ^= q & 0x80 ? 0x09 : 0;
+
+    /* compute the affine transformation */
+    uint8_t xformed = q ^ ROTL8(q, 1) ^ ROTL8(q, 2) ^ ROTL8(q, 3) ^ ROTL8(q, 4);
+
+    sbox[p] = xformed ^ 0x63;
+  } while (p != 1);
+
+  /* 0 is a special case since it has no inverse */
+  sbox[0] = 0x63;
+}
+
 
 #endif // AES_IMPLEMENTATION
