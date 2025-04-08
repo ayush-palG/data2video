c012f3f864f1b441b4bcb641d821720abab8cac3
ayush-palG <ayushpal.9470@gmail.com>
2025-04-08 13:48:50 +0530

    (#1) Initial Setup for aes header-only

---
+++ AES.h
@@ -0,0 +1,16 @@
+#ifndef AES_H_
+#define AES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+#endif // AES_H_
+
+#ifdef AES_IMPLEMENTATION
+
+
+
+#endif // AES_IMPLEMENTATION
