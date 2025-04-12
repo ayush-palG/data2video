c9d32c933927785bc7702a5d524c496e09314f99
ayush-palG <ayushpal.9470@gmail.com>
2025-04-12 06:23:47 +0530

    (#5) Example for byte_to_sv and sv_to_byte

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 int main(void)
 {
-  get_huffman_tree("input");
+  String_View sv = {0};
+  sv.str = (char *) malloc(sizeof(char) * U8_CAPACITY);
+
+  byte_to_sv(&sv, 'a');
+  printf("%.*s\n", (int) sv.size, sv.str);
+
+  uint8_t byte = 0;
+  if (!sv_to_byte(&sv, &byte)) {
+    printf("sv have less than 8 bits to work with!\n");
+  }
+  printf("%c\n", byte);
   
   return 0;
 }
