645ce29468c8773bd344d935ec5dc0df8b200694
ayush-palG <ayushpal.9470@gmail.com>
2025-04-18 11:54:14 +0530

    (#5) Add capacity member into String_View Structure

--- src/compression_algorithms/SV.h
+++ src/compression_algorithms/SV.h
@@ -1,13 +1,14 @@
 #ifndef SV_H_
 #define SV_H_
 
-// TODO: See if we want capacity to be the member of String_View structure
 typedef struct {
   char *str;
   size_t size;
+  size_t capacity;
 } String_View;
 
 // TODO: sv_alloc would return String_View*
+// TODO: If sv_alloc returns String_View*, then create a func to free the allocated memory
 void sv_alloc(String_View *sv, size_t size);
 
 void sv_rev(String_View *sv, size_t start_pos, size_t end_pos);
@@ -32,6 +33,7 @@ void sv_alloc(String_View *sv, size_t size)
   *sv = (String_View) {
     .str = (char *) malloc(sizeof(char) * size),
     .size = 0,
+    .capacity = size,
   };
 }
 
@@ -56,7 +58,7 @@ void sv_rev_bytes(String_View *sv)
 
 void sv_concat_sv(String_View *dst, String_View *src)
 {
-  assert(dst->size + src->size < SV_CAPACITY);
+  assert(dst->size + src->size <= dst->capacity);
   for (size_t i = 0; i < src->size; ++i) {
     dst->str[dst->size++] = src->str[i];
   }
@@ -67,11 +69,13 @@ void sv_concat_file(FILE *file, String_View *src)
   sv_rev_bytes(src);
   uint8_t byte = 0;
   while (src->size >= U8_SIZE) {
+    assert(src->size + U8_SIZE <= src->capacity);
     sv_bits_to_byte(src, &byte);
     fwrite(&byte, sizeof(byte), 1, file);
   }
 }
 
+// TODO: Instead of adding the bits at the end of sv, alloc a new sv and add to it them.
 void sv_byte_to_bits(String_View *sv, uint8_t byte)
 {
   for (size_t i = 0; i < U8_SIZE; ++i) {
@@ -79,6 +83,7 @@ void sv_byte_to_bits(String_View *sv, uint8_t byte)
   }
 }
 
+// TODO: there is no need to spit out bool here
 bool sv_bits_to_byte(String_View *sv, uint8_t *byte)
 {
   if (sv->size >= U8_SIZE) {
@@ -87,6 +92,7 @@ bool sv_bits_to_byte(String_View *sv, uint8_t *byte)
       assert(sv->str[i] == '0' || sv->str[i] == '1');
       *byte = (*byte << 1) | (sv->str[i] - '0');
     }
+    // TODO: To make it more general, remove the following line and add it to where it needed
     sv->size -= U8_SIZE;
     return true;
   }
